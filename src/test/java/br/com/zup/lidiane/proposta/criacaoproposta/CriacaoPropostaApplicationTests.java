6c9ea76178d0d6cd02b531677981e8c68f3f3b7a
Lidiane <lidiane.santos@zup.com.br>
2021-06-04 10:19:40 -0300

    Setup Proposta

---
+++ src/test/java/br/com/zup/lidiane/proposta/criacaoproposta/CriacaoPropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.lidiane.proposta.criacaoproposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriacaoPropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
